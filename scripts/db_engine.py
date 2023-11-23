49278004e98c07b688b324470ce5c927b5dce787
ltvinh <everwellmax@gmail.com>
2023-11-23 21:50:11 +0700

    Refactor database connection string

--- scripts/db_engine.py
+++ scripts/db_engine.py
@@ -1,12 +1,6 @@
-from sqlalchemy import URL, create_engine
+from sqlalchemy import create_engine
 
-connection_string = URL.create(
-  'postgresql',
-  username='lgthevinh', 
-  password='4bokUp9IiNVG', 
-  host='ep-solitary-haze-93016686.ap-southeast-1.aws.neon.tech', 
-  database='data_engineer', 
-)
+connection_string = 'postgresql://lgthevinh:4bokUp9IiNVG@ep-solitary-haze-93016686.ap-southeast-1.aws.neon.tech/data_engineer?sslmode=require'
 
 def db_engine():
   return create_engine(connection_string)
